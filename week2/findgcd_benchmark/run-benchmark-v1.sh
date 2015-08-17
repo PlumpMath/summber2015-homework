d822573ec831281349e961fbfc815f5165a57b96
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-08-17 08:48:43 +0800

    clear run-benchmark script
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- week2/findgcd_benchmark/run-benchmark-v1.sh
+++ week2/findgcd_benchmark/run-benchmark-v1.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-FINDGCD=./findGCD_v1
-
-for (( i = 2; i <= 9999; i++ )); do
-    $FINDGCD $i  | awk '{print $7 " " $11 }'
-done
+source scripts/run-benchmark.sh
+run_benchmark ./findGCD_v1
