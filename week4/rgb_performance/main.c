9822648c705df19690ec46bf44e90b76d62d28a9
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-08-24 23:20:14 +0800

    rgb: add simple read png file support
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- week4/rgb_performance/main.c
+++ week4/rgb_performance/main.c
@@ -4,10 +4,18 @@
 #define PNG_DEBUG 3
 #include <png.h>
 
-#include "rgb.h"
+/* some file local variable to save tmp image */
+static int width = 0;
+static int height = 0;
+
+static png_bytep *row_pointers;
+static png_byte color_type;
+static png_byte bit_depth;
+
 
 /* original version */
-void rgba_to_bw(uint32_t *bitmap, int width, int height, long stride) {
+void rgba_to_bw(uint32_t *bitmap, int width, int height, long stride)
+{
         int row, col;
         uint32_t pixel, r, g, b, a, bw;
         for (row = 0; row < height; row++) {
@@ -23,8 +31,9 @@ void rgba_to_bw(uint32_t *bitmap, int width, int height, long stride) {
         }
 }
 
-/* optimize version */
-void rgba_to_bw_v1(uint32_t *bitmap, int width, int height, long stride) {
+/* optimize version v1 */
+void rgba_to_bw_v1(uint32_t *bitmap, int width, int height, long stride)
+{
         int row, col;
         uint32_t pixel, r, g, b, a, bw;
         for (row = height; row != 0; row--) {
@@ -41,8 +50,118 @@ void rgba_to_bw_v1(uint32_t *bitmap, int width, int height, long stride) {
         }
 }
 
+void read_image(const char* filename)
+{
+        FILE *fp = fopen(filename, "rb");
+        if (!fp) {
+                perror("open file");
+                return;
+        }
+
+        png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+        if(!png) abort();
+
+        png_infop info = png_create_info_struct(png);
+        if(!info) abort();
+
+        if(setjmp(png_jmpbuf(png))) abort();
+
+        png_init_io(png, fp);
+
+        png_read_info(png, info);
+
+        width      = png_get_image_width(png, info);
+        height     = png_get_image_height(png, info);
+        color_type = png_get_color_type(png, info);
+        bit_depth  = png_get_bit_depth(png, info);
+
+        // Read any color_type into 8bit depth, RGBA format.
+        // See http://www.libpng.org/pub/png/libpng-manual.txt
+
+        if(bit_depth == 16)
+                png_set_strip_16(png);
+
+        if(color_type == PNG_COLOR_TYPE_PALETTE)
+                png_set_palette_to_rgb(png);
+
+        // PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
+        if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+                png_set_expand_gray_1_2_4_to_8(png);
+
+        if(png_get_valid(png, info, PNG_INFO_tRNS))
+                png_set_tRNS_to_alpha(png);
+
+        // These color_type don't have an alpha channel then fill it with 0xff.
+        if(color_type == PNG_COLOR_TYPE_RGB ||
+           color_type == PNG_COLOR_TYPE_GRAY ||
+           color_type == PNG_COLOR_TYPE_PALETTE)
+                png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+
+        if(color_type == PNG_COLOR_TYPE_GRAY ||
+           color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+                png_set_gray_to_rgb(png);
+
+        png_read_update_info(png, info);
+
+        row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
+        for(int y = 0; y < height; y++) {
+                row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png,info));
+        }
+
+        png_read_image(png, row_pointers);
+
+        fclose(fp);
+}
+
+void write_image(const char *filename)
+{
+        FILE *fp = fopen(filename, "wb");
+        if (!fp) {
+                perror("write file");
+                exit(-1);
+        }
+
+        png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+        if (!png) abort();
+
+        png_infop info = png_create_info_struct(png);
+        if (!info) abort();
+
+        if (setjmp(png_jmpbuf(png))) abort();
+
+        png_init_io(png, fp);
+
+        // Output is 8bit depth, RGBA format.
+        png_set_IHDR (
+                png,
+                info,
+                width, height,
+                8,
+                PNG_COLOR_TYPE_RGBA,
+                PNG_INTERLACE_NONE,
+                PNG_COMPRESSION_TYPE_DEFAULT,
+                PNG_FILTER_TYPE_DEFAULT);
+        png_write_info(png, info);
+
+        png_write_image(png, row_pointers);
+        png_write_end(png, NULL);
+
+        for(int y = 0; y < height; y++) {
+                free(row_pointers[y]);
+        }
+        free(row_pointers);
+
+        fclose(fp);
+}
+
+void process_image()
+{
+}
+
 int main(int argc, char *argv[])
 {
-
+        read_image("sample.png");
+        process_image();
+        write_image("sample_grey.png");
         return 0;
 }
